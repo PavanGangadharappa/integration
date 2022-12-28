c758fb8b5cc23d18e6a3a650236cacccdc03b10f
PavanGangadharappa <118874302+PavanGangadharappa@users.noreply.github.com>
2022-12-29 00:49:42 +0530

    Add files via upload

---
+++ src/main/java/com/pavan/nexus_project/NexusProjectApplication.java
@@ -0,0 +1,13 @@
+package com.pavan.nexus_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexusProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexusProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pavan/nexus_project/NexusProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pavan.nexus_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexusProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
